487141931c4e8c9df5759bf400e9c1dedd667093
dx._.xk7 <polcet112@gmail.com>
2024-11-14 00:33:25 +0900

    기초 세팅

---
+++ Wednesday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wednesday
+//
+//  Created by 이득령 on 11/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        print("called ViewController - run App")
+    }
+
+}
+
